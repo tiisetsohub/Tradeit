184bb25b15cdd57f7cc78ec1b9ba3ccd746b4944
tiisetso <tiimojalefa@gmail.com>
2022-04-09 00:19:18 +0200

    database

--- tradeit/src/App.js
+++ tradeit/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Home from './Pages/Home'
+import Login from './Pages/Login'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
             <Home />
+        </Route>
+
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </Router>
--- tradeit/src/Pages/Home.js
+++ tradeit/src/Pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useState, useEffect } from 'react';
-import { db } from '../firebase-config'
-import { collection, getDocs, addDoc } from "firebase/firestore"
+import { db } from '../firebase-config';
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './Home.css';
+import Login from './Login';
+
+
 
 
 export default function Home() {
@@ -21,15 +26,53 @@ export default function Home() {
 
     return (
         <div>
-            {Inventory.map((item) => {
-                return <div>
-                    <h1>NAME: {item.Name}</h1>
-                    <h1>desc: {item.Description}</h1>
-                    <h1>price: {item.Price}</h1>
-                    <h1>quantity: {item.Quantity}</h1>
-                </div>
-            })}
+            <Navbar />
+            <div className="bodydiv">
+                {Inventory.map((item) => {
+                    return <div className="itemdiv">
+                        <img src={item.Image} alt="nope" />
+                        <div className="textdiv">
+                            <h1 className="itemname">{item.Name}</h1>
+                        </div>
+                        <h1 className="itemprice">R{item.Price}</h1>
+                    </div>
+                })}
+            </div>
         </div>
     );
 
 }
+
+function Navbar(){
+    const [showLinks, setShowLinks] = useState(false);
+    return (
+        <div className="navbar">
+            <div className="leftside">
+                <div className="links" id = {showLinks ? "hidden" : ""}>
+                    <Link className="navlink" to='/login'>
+                        <p>Login/Signup</p>
+                    </Link>
+                    <Link className="navlink" to='/login'>
+                        <p>Home</p>
+                    </Link>
+                    <Link className="navlink" to='/login'>
+                        <p>About</p>
+                    </Link>
+                    <Link className="navlink" to='/login'>
+                        <p>Contact</p>
+                    </Link>
+                </div>
+                <button onClick={() => setShowLinks(!showLinks)}>
+                    ≡
+                </button>
+            </div>
+            <div className="rightside"> 
+                <input className="edtsearch" placeholder="Search"/>
+                <button className="btnsearch">
+                    Search
+                </button>
+            </div>
+        </div>
+    )
+}
+
